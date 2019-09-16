3c477b87451428dfcc2a1dc405aff94d8b91df87
Arthur Neto <arthursan.neto@gmail.com>
2019-09-16 01:00:10 -0300

    Comentando classes de testes exemplo

--- src/server/ifsc.tcc.Portal.Tests/Api/Controllers/FeatureExampleControllerTests.cs
+++ src/server/ifsc.tcc.Portal.Tests/Api/Controllers/FeatureExampleControllerTests.cs
@@ -1,149 +1,138 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
-using NUnit.Framework;
-using ifsc.tcc.Portal.Api.Controllers;
-using ifsc.tcc.Portal.Application.FeatureExampleModule;
-using ifsc.tcc.Portal.Application.FeatureExampleModule.Models.Commands;
-using ifsc.tcc.Portal.Domain.FeatureExampleModule;
-
-namespace ifsc.tcc.Portal.Tests.Api.Controllers
+﻿namespace ifsc.tcc.Portal.Tests.Api.Controllers
 {
-    [TestFixture]
-    public class FeatureExampleControllerTests
-    {
-        private readonly Mock<IFeatureExampleAppService> _moqFeatureExampleAppService;
+    //[TestFixture]
+    //public class FeatureExampleControllerTests
+    //{
+    //    private readonly Mock<IFeatureExampleAppService> _moqFeatureExampleAppService;
 
-        public FeatureExampleControllerTests()
-        {
-            _moqFeatureExampleAppService = new Mock<IFeatureExampleAppService>(MockBehavior.Strict);
-        }
+    //    public FeatureExampleControllerTests()
+    //    {
+    //        _moqFeatureExampleAppService = new Mock<IFeatureExampleAppService>(MockBehavior.Strict);
+    //    }
 
-        [Test]
-        public async Task Should_ReturnOk_When_RetrieveAll()
-        {
-            // Arrange
-            _moqFeatureExampleAppService
-                .Setup(x => x.RetrieveAllAsync())
-                .ReturnsAsync(It.IsNotNull<IEnumerable<FeatureExample>>())
-                .Verifiable();
+    //    [Test]
+    //    public async Task Should_ReturnOk_When_RetrieveAll()
+    //    {
+    //        // Arrange
+    //        _moqFeatureExampleAppService
+    //            .Setup(x => x.RetrieveAllAsync())
+    //            .ReturnsAsync(It.IsNotNull<IEnumerable<FeatureExample>>())
+    //            .Verifiable();
 
-            // Action
-            var controller = GetController();
+    //        // Action
+    //        var controller = GetController();
 
-            var result = await controller.RetrieveAllAsync();
+    //        var result = await controller.RetrieveAllAsync();
 
-            // Assert
-            var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
-            okObjectResult.StatusCode.Should().Be(200);
+    //        // Assert
+    //        var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+    //        okObjectResult.StatusCode.Should().Be(200);
 
-            _moqFeatureExampleAppService.Verify();
-        }
+    //        _moqFeatureExampleAppService.Verify();
+    //    }
 
-        [Test]
-        public async Task Should_ReturnOk_When_RetrieveByID()
-        {
-            // Arrange
-            var id = 1;
+    //    [Test]
+    //    public async Task Should_ReturnOk_When_RetrieveByID()
+    //    {
+    //        // Arrange
+    //        var id = 1;
 
-            _moqFeatureExampleAppService
-                .Setup(x => x.RetrieveByIDAsync(id))
-                .ReturnsAsync(It.IsNotNull<FeatureExample>())
-                .Verifiable();
+    //        _moqFeatureExampleAppService
+    //            .Setup(x => x.RetrieveByIDAsync(id))
+    //            .ReturnsAsync(It.IsNotNull<FeatureExample>())
+    //            .Verifiable();
 
-            // Action
-            var controller = GetController();
+    //        // Action
+    //        var controller = GetController();
 
-            var result = await controller.RetrieveByIDAsync(id);
+    //        var result = await controller.RetrieveByIDAsync(id);
 
-            // Assert
-            var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
-            okObjectResult.StatusCode.Should().Be(200);
+    //        // Assert
+    //        var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+    //        okObjectResult.StatusCode.Should().Be(200);
 
-            _moqFeatureExampleAppService.Verify();
-        }
+    //        _moqFeatureExampleAppService.Verify();
+    //    }
 
-        [Test]
-        public async Task Should_ReturnOk_When_Add()
-        {
-            // Arrange
-            var command = new FeatureExampleAddCommand()
-            {
-                FeatureExampleType = FeatureExampleEnum.EnumExample,
-            };
+    //    [Test]
+    //    public async Task Should_ReturnOk_When_Add()
+    //    {
+    //        // Arrange
+    //        var command = new FeatureExampleAddCommand()
+    //        {
+    //            FeatureExampleType = FeatureExampleEnum.EnumExample,
+    //        };
 
-            _moqFeatureExampleAppService
-                .Setup(x => x.AddAsync(command))
-                .ReturnsAsync(true)
-                .Verifiable();
+    //        _moqFeatureExampleAppService
+    //            .Setup(x => x.AddAsync(command))
+    //            .ReturnsAsync(true)
+    //            .Verifiable();
 
-            // Action
-            var controller = GetController();
+    //        // Action
+    //        var controller = GetController();
 
-            var result = await controller.AddAsync(command);
+    //        var result = await controller.AddAsync(command);
 
-            // Assert
-            var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
-            okObjectResult.StatusCode.Should().Be(200);
+    //        // Assert
+    //        var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+    //        okObjectResult.StatusCode.Should().Be(200);
 
-            _moqFeatureExampleAppService.Verify();
-        }
+    //        _moqFeatureExampleAppService.Verify();
+    //    }
 
-        [Test]
-        public async Task Should_ReturnOk_When_Update()
-        {
-            // Arrange
-            var command = new FeatureExampleUpdateCommand()
-            {
-                ID = 1,
-                FeatureExampleType = FeatureExampleEnum.EnumExample,
-            };
+    //    [Test]
+    //    public async Task Should_ReturnOk_When_Update()
+    //    {
+    //        // Arrange
+    //        var command = new FeatureExampleUpdateCommand()
+    //        {
+    //            ID = 1,
+    //            FeatureExampleType = FeatureExampleEnum.EnumExample,
+    //        };
 
-            _moqFeatureExampleAppService
-                .Setup(x => x.UpdateAsync(command))
-                .ReturnsAsync(true)
-                .Verifiable();
+    //        _moqFeatureExampleAppService
+    //            .Setup(x => x.UpdateAsync(command))
+    //            .ReturnsAsync(true)
+    //            .Verifiable();
 
-            // Action
-            var controller = GetController();
+    //        // Action
+    //        var controller = GetController();
 
-            var result = await controller.UpdateAsync(command);
+    //        var result = await controller.UpdateAsync(command);
 
-            // Assert
-            var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
-            okObjectResult.StatusCode.Should().Be(200);
+    //        // Assert
+    //        var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+    //        okObjectResult.StatusCode.Should().Be(200);
 
-            _moqFeatureExampleAppService.Verify();
-        }
+    //        _moqFeatureExampleAppService.Verify();
+    //    }
 
-        [Test]
-        public async Task Should_ReturnOk_When_Remove()
-        {
-            // Arrange
-            var id = 1;
+    //    [Test]
+    //    public async Task Should_ReturnOk_When_Remove()
+    //    {
+    //        // Arrange
+    //        var id = 1;
 
-            _moqFeatureExampleAppService
-                .Setup(x => x.RemoveAsync(id))
-                .ReturnsAsync(true)
-                .Verifiable();
+    //        _moqFeatureExampleAppService
+    //            .Setup(x => x.RemoveAsync(id))
+    //            .ReturnsAsync(true)
+    //            .Verifiable();
 
-            // Action
-            var controller = GetController();
+    //        // Action
+    //        var controller = GetController();
 
-            var result = await controller.RemoveAsync(id);
+    //        var result = await controller.RemoveAsync(id);
 
-            // Assert
-            var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
-            okObjectResult.StatusCode.Should().Be(200);
+    //        // Assert
+    //        var okObjectResult = result.Should().BeOfType<OkObjectResult>().Subject;
+    //        okObjectResult.StatusCode.Should().Be(200);
 
-            _moqFeatureExampleAppService.Verify();
-        }
+    //        _moqFeatureExampleAppService.Verify();
+    //    }
 
-        private FeatureExampleController GetController()
-        {
-            return new FeatureExampleController(_moqFeatureExampleAppService.Object);
-        }
-    }
+    //    private FeatureExampleController GetController()
+    //    {
+    //        return new FeatureExampleController(_moqFeatureExampleAppService.Object);
+    //    }
+}
 }
