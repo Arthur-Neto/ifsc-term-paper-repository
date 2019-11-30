fdbce8be2a1b34b0aaeb87b9e5f97c7ea7ed4fca
Arthur Neto <arthursan.neto@gmail.com>
2019-11-30 10:39:02 -0300

    Configurações

---
+++ src/server/ifsc.tcc.Portal.Api/Controllers/PublicController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ifsc.tcc.Portal.Api.Controllers
+{
+    [ApiController]
+    [Route("api/public")]
+    public class PublicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult IsAlive()
+        {
+            return Ok(true);
+        }
+    }
+}
